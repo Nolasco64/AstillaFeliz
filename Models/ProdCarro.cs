0e28c7c2b478af909020ee8a829fb4bf09836adc
LeslyCruz2 <94932245+LeslyCruz2@users.noreply.github.com>
2021-11-24 02:17:36 -0600

    Add files via upload

---
+++ Models/ProdCarro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarpinteriaProyecto2.Models
+{
+    public class ProdCarro
+    {
+        private contextCarpinteriaProyecto db = new contextCarpinteriaProyecto();
+        private List<Productos> products;
+        public ProdCarro()
+        {
+            products = db.Productos.ToList();
+        }
+
+        public List<Productos> findAll()
+        {
+            return this.products;
+        }
+
+        public Productos find(int id)
+        {
+            Productos pp = this.products.Single(p => p.Id_producto.Equals(id));
+            return pp;
+        }
+    }
+}
